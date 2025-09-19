4665e4ef426e2151ed9ac5443233997807240f9a
hchubaldo <39108735+hchubaldo@users.noreply.github.com>
2025-09-19 01:58:02 -0500

    removed lines that were being used to test locally

--- assignment2.py
+++ assignment2.py
@@ -11,9 +11,6 @@ X = assign2_df_train.drop(columns=['meal','id','DateTime'])
 assign2_df_test = pd.read_csv('assignment2test.csv')
 xt = assign2_df_test.drop(columns=['meal','id','DateTime'])
 
-assign2_df_truth = pd.read_csv('/Users/hugocorado/Documents/GitHub/econ8310-assignment2/tests/testData.csv')
-yt = assign2_df_truth['meal']
-
 num_classes_train = len(np.unique(Y))
 
 model = XGBClassifier(n_estimators=50, max_depth=3, learning_rate=0.5, objective='multi:softmax', num_class=num_classes_train)
@@ -21,5 +18,3 @@ model = XGBClassifier(n_estimators=50, max_depth=3, learning_rate=0.5, objective
 modelFit = model.fit(X, Y)
 
 pred = modelFit.predict(xt)
-
-# forgot to activate workflow
